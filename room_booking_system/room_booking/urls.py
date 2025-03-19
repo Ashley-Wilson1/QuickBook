928584d270e07fc8e7c3a758d4f5dff6417604cc
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-03-19 13:47:44 +0000

    Added booklist to dashboard
    added a view for booking list and changed url to point to this view

--- room_booking_system/room_booking/urls.py
+++ room_booking_system/room_booking/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import BookingList
 
 urlpatterns = [
-    path('',views.dashboard, name="dashboard"),
+    path('',BookingList.as_view(),name="dashboard"),
 ]
--- room_booking_system/room_booking/views.py
+++ room_booking_system/room_booking/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, FormView,DetailView
+from .models import Room, RoomBooking
 
-def dashboard(request):
-    return render(request, 'dashboard.html', {})
+# def dashboard(request):
+#     return render(request, 'dashboard.html', {})
+
+class BookingList(ListView):
+    model=RoomBooking
+    template_name = 'dashboard.html'
+    def get_queryset(self, *args, **kwargs):
+        if self.request.user.is_staff: #super user also staff
+            booking_list= RoomBooking.objects.all()
+            return booking_list
+        else:
+            booking_list = RoomBooking.objects.filter(user=self.request.user) # only returns users bookings
+            return booking_list
