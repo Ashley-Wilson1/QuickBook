b277afc821c4bce2c0671b8bee3c5b2833664026
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-03-19 22:49:30 +0000

    Added more urls to handle JWT tokens

--- room_booking_system/room_booking_system/urls.py
+++ room_booking_system/room_booking_system/urls.py
@@ -1,10 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from members.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('room_booking.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/',include('members.urls')),
+    path('members/user/register/', CreateUserView.as_view(), name = 'register'),
+    path('members/token/', TokenObtainPairView.as_view(), name = 'get_token'),
+    path('members/token/refresh/', TokenRefreshView.as_view(),name='refresh'),
+    path('members-auth/', include('rest_framework.urls')),
 ]
