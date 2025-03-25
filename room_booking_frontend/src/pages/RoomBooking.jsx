15ee27b65ce8f415bd1a74af530167d9967a38cc
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-03-25 18:48:26 +0000

    Created empty room booking form

--- room_booking_frontend/src/pages/RoomBooking.jsx
+++ room_booking_frontend/src/pages/RoomBooking.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavBar";
+import RoomBookingForm from "../components/RoomBookingForm";
 
 function NotFound() {
 	return (
 		<div>
 			<NavBar />
 			<h1>Book a Room</h1>
+			<RoomBookingForm />
 		</div>
 	);
 }
