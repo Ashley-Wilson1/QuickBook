c837379ce467ff0adc62a608d4ce7da335a47606
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-03-25 13:45:00 +0000

    Added username/email checks in serializers

--- room_booking_system/members/serializers.py
+++ room_booking_system/members/serializers.py
@@ -7,6 +7,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'user_type', 'password']
         extra_kwargs = {"password": {"write_only": True}}
 
+    def validate_username(self, value):
+        """Check if the username already exists."""
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("This username is already taken.")
+        return value
+
+    def validate_email(self, value):
+        """Check if the email is already registered."""
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already registered.")
+        return value
+
     def create(self, validated_data):
         password = validated_data.pop("password", None)
         user = User(**validated_data)
