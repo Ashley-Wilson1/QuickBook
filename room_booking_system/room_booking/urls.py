488c77dbec26bc69ab5168c4f84734133b0b837b
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-03-25 20:48:35 +0000

    Added view and endpoint to get users bookings

--- room_booking_system/room_booking/urls.py
+++ room_booking_system/room_booking/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-# from .views import BookingList, BookingDetailView, CancelBooking, CreateBooking
-from room_booking.views import CreateBooking, BookingDelete, RoomListView
+from room_booking.views import CreateBooking, BookingDelete, RoomListView, UserBookingsView
 
 urlpatterns = [
-    # path('',BookingList.as_view(),name="dashboard"),
-    # path('booking/<int:pk>', BookingDetailView.as_view(), name = "booking-detail"),
-    # path('cancel/<int:booking_id>/', CancelBooking, name='cancel_booking'),
-    # path('create_booking/', CreateBooking.as_view(), name = 'create_booking'),
     path('bookings/',CreateBooking.as_view(),name='booking-list'),
     path('bookings/delete/<int:pk>', BookingDelete.as_view(),name='delete-booking'),
     path("rooms/", RoomListView.as_view(), name="room-list"),
+    path('user/bookings/', UserBookingsView.as_view(), name='user-bookings'),
+
 ]
