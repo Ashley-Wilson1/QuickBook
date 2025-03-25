504aec4e9a9c54a345865afce0a7d92e440967a7
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-03-25 21:00:50 +0000

    Added css styles to bookingList

--- room_booking_frontend/src/components/BookingList.jsx
+++ room_booking_frontend/src/components/BookingList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/BookingList.css";
 
 function BookingList({ bookings }) {
 	return (
--- room_booking_frontend/src/pages/Home.jsx
+++ room_booking_frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
 	return (
 		<div>
 			<NavBar />
-			<h1> Dashboard</h1>
+
 			<UserBookings />
 		</div>
 	);
