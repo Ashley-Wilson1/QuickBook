6e012a3228abf7b0286276207076165d9fbf0d1f
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-03-26 19:22:56 +0000

    Created chat model

--- room_booking_system/chat/models.py
+++ room_booking_system/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from room_booking.models import RoomBooking  
 
-# Create your models here.
+
+User = get_user_model()
+
+class Message(models.Model):
+    booking = models.ForeignKey(RoomBooking, on_delete=models.CASCADE, related_name="messages")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.text[:20]}..."
