43473d30c7845c0680f124f35d0cbf86fa7d7daf
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-03-27 20:34:10 +0000

    Created notifications database

--- room_booking_system/notifications/models.py
+++ room_booking_system/notifications/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    read_status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    notification_type = models.CharField(max_length=50, choices=[('booking', 'Booking'), ('message', 'Message')])
+
--- room_booking_system/room_booking_system/settings.py
+++ room_booking_system/room_booking_system/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'room_booking',
     'chat',
+    'notifications',
     'members',
 ]
 
